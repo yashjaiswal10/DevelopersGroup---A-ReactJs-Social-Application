c52f856e8d7ff0b64e34804932a3473ff04a56e6
Yash <yashjaiswal1097@gmail.com>
2018-12-24 19:24:02 +0530

    error fix

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
- const express= require('express');
- const app= express();
-
- const cors = require('cors');
+const express= require('express');
+const app= express();
+const path=require('path');
+const cors = require('cors');
 app.use(cors());
 
 //  Body parser
- const bodyParser=require("body-parser");
- app.use(bodyParser.urlencoded({extended:false}));
- app.use(bodyParser.json());
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 
- const mongoose=require("mongoose");
- const db=require("./config/keys").mongoURI;
+const mongoose=require("mongoose");
+const db=require("./config/keys").mongoURI;
 
- const passport=require("passport");
+const passport=require("passport");
 
 
 const posts=require("./routes/api/posts")
 const users=require("./routes/api/users")
 const profile=require("./routes/api/profile")
 
- mongoose
- .connect(db)
- .then(()=>console.log("db connected"));
+mongoose
+.connect(db)
+.then(()=>console.log("db connected"));
 
- app.get('/',(req,res)=>res.send("Hello"));
+//  app.get('/',(req,res)=>res.send("Hello"));
 // passport middleware
 
 // passport config
@@ -32,11 +32,20 @@ require('./config/passport')(passport);
 
 
 
- app.use("/api/users",users);
- app.use("/api/profile",profile);
- app.use("/api/posts",posts);
+app.use("/api/users",users);
+app.use("/api/profile",profile);
+app.use("/api/posts",posts);
 
- const port=process.env.PORT||8000;
+// Server static asset if in production
+if(process.env.NODE_ENV==='production')
+{
+   app.use(express.static('client/build'));
+   app.get('*',(req,res)=>{
+       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+   });
+} 
 
- app.listen(port,()=>console.log('server started on port '+port));
+const port=process.env.PORT||8000;
+
+app.listen(port,()=>console.log('server started on port '+port));
 
