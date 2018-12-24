838464661b9f422424d2984e0d0d8d6030a492b3
Yash <yashjaiswal1097@gmail.com>
2018-12-24 20:05:05 +0530

    display fix1

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,20 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())||compose
-  )
-);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())||compose
+//   )
+// );
+
+import { createStore, applyMiddleware, compose } from 'redux';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, preloadedState, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 export default store;
